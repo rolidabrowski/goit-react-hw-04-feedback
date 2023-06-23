f644320290e4d96293c47af9c3fff9b0e3dbdcb2
rolidabrowski <116666790+rolidabrowski@users.noreply.github.com>
2023-06-23 17:04:36 +0000

    Deploying to gh-pages from @ rolidabrowski/goit-react-hw-04-feedback@e0824e37a6b4144ee3e57dfae2e9cd92f4b3a941 🚀

--- src/components/FeedbackOptions/index.js
+++
@@ -1,1 +0,0 @@
-export { FeedbackOptions } from './FeedbackOptions';
--- src/components/Section/Section.jsx
+++
@@ -1,8 +0,0 @@
-import css from './Section.module.css';
-
-export const Section = ({ title, children }) => (
-  <section>
-    <h2 className={css.title}>{title}</h2>
-    {children}
-  </section>
-);
